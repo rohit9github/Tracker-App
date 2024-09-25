89a1de3d9101ed7ab32f9f456774478d896f2188
rohit9github <gohilrohit@gmail.com>
2024-09-25 22:16:30 +0530

    create server

---
+++ back-end/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const colors = require('colors');
+// config dot env file
+dotenv.config()
+
+// rest object
+const app = express();
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+// routes 
+app.get('/',(req,res)=>{
+    res.send('<h1>hello</h1>')
+})
+
+//port
+const PORT = 8080 || process.env.PORT
+
+//L listen port
+app.listen(PORT,()=>{
+    console.log(`server is running ${PORT}`);
+    
+})
