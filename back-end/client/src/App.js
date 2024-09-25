b33bb4934dcdf5db99b99b7d0dbb6b681a5062db
rohit9github <gohilrohit@gmail.com>
2024-09-26 00:41:07 +0530

    set-up front-end

---
+++ back-end/client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center">Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
